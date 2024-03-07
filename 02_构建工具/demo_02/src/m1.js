5785d996683fdc874d3535e0e05687fcbd8d71aa
liudongdong <2659891451@qq.com>
2024-03-07 11:19:08 +0800

    初始化文档

---
+++ 02_构建工具/demo_02/src/m1.js
@@ -0,0 +1,8 @@
+// import $ from "jquery"
+
+export default {
+    setH1(){
+        document.body.insertAdjacentHTML("beforeend", "<h1>你好！没毛病</h1>")
+        // $("body").append("<h1>你好！秋冬</h1>")
+    }
+}
